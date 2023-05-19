6839c4903f17c02ceb069530515e3348816a781c
Stefan Spasovski <mefo_mzt@live.com>
2023-05-19 16:34:44 +0200

    changes

--- Greetings/Greeting.sol
+++ Greetings/Greeting.sol
@@ -2,14 +2,11 @@
 
 pragma solidity ^0.8.9;
 
-//Importing the console.log package
-import "../node_modules/hardhat/console.sol";
 
 contract Greeter {
     string private greeting;
 
     constructor(string memory defaultMessage){
-       console.log("Deploying the Greeter smart contract", defaultMessage);
        greeting = defaultMessage;
     }
 
@@ -20,7 +17,6 @@ contract Greeter {
 
     // Function: this will allow us to change the greeting message
     function setGreeting(string memory newMessage) public {
-        console.log("Changing the Greeting message from %s to %s", greeting, newMessage);
         greeting = newMessage;
     }
 
